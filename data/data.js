eadf1a64935731361511dff0188654e97f0d5e48
Sadharan Ladka <debdutbhatta2007@gmail.com>
2024-07-03 20:36:55 +0530

    Basic File Structure

---
+++ data/data.js
@@ -0,0 +1,45 @@
+function Greet() {
+    alert("Hello, World");
+}
+
+// for each square
+function Square(color, id, piece) {
+    return {
+        color,
+        id,
+        piece
+    };
+}
+
+function SquareRaw(rowId) {
+
+    const squareRow = [];
+    const abcd = ["a","b","c","d","e","f", "g","h"]
+
+    if (rowId % 2 == 0) {
+        abcd.forEach((element, index) => {
+            if(index % 2 == 0){
+                squareRow.push(Square("white", "e5", null))
+            }else{
+                squareRow.push(Square("black", "e5", null))
+            }
+        });
+    } else {
+        squareRow.push(Square("black", "e5", null))
+    }
+    return squareRow;
+}
+
+function initGame() {
+
+    return [SquareRaw(8),
+        SquareRaw(7), 
+        SquareRaw(6), 
+        SquareRaw(5), 
+        SquareRaw(4), 
+        SquareRaw(3), 
+        SquareRaw(2), 
+        SquareRaw(1)]
+}
+
+export { initGame };
---
+++ index.js
@@ -0,0 +1,3 @@
+import { initGame } from "./data/data.js";
+
+console.log(initGame());
